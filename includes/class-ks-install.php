b8949326987ded5a025161de5dc67bc5d10c342b
Kakoma <kakomap@gmail.com>
2014-08-23 21:13:39 +0300

    Better OO design

---
+++ includes/class-ks-install.php
@@ -0,0 +1,160 @@
+<?php
+/** 
+ * All things installation
+ */
+ 
+ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if ( ! class_exists( 'KS_Install' ) ) :
+
+
+ class KS_Install{
+ 
+ public function __construct() {
+		register_activation_hook( KS_PLUGIN_FILE, array( $this, 'install_ks' ) );
+
+		//add_action( 'admin_init', array( $this, 'install_actions' ) );
+		//add_action( 'admin_init', array( $this, 'check_version' ), 5 );
+		//add_action( 'in_plugin_update_message-woocommerce/woocommerce.php', array( $this, 'in_plugin_update_message' ) );
+		
+		add_action('admin_menu',  array( $this,'kanzu_support_menu_pages'/0);
+		add_filter('plugin_action_links',  array( $this,'kanzu_support_plugin_action_links'), 10, 2);
+	}
+ 
+ function install_ks(){
+	$this->ks_create_tables();
+
+ }
+ 
+ /**
+  * Install Kanzu Support
+  */
+   function ks_create_tables() {
+            global $wpdb;        
+			$wpdb->hide_errors();		            
+             
+			require_once(ABSPATH . 'wp-admin/includes/upgrade.php'); 
+
+                $kanzusupport_tables = "
+				CREATE TABLE `{$wpdb->prefix}kanzusupport_tickets` (
+				`primkey` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+				`title` VARCHAR(512) NOT NULL, `initial_message` TEXT NOT NULL, 
+				`user_id` INT NOT NULL, `email` VARCHAR(256) NOT NULL, 
+				`assigned_to` INT NOT NULL DEFAULT '0', 
+				`severity` VARCHAR(64) NOT NULL, 
+				`resolution` VARCHAR(64) NOT NULL, 
+				`time_posted` VARCHAR(128) NOT NULL, 
+				`last_updated` VARCHAR(128) NOT NULL, 
+				`last_staff_reply` VARCHAR(128) NOT NULL, 
+				`target_response_time` VARCHAR(128) NOT NULL,
+                `type` VARCHAR( 255 ) NOT NULL
+				);	
+				CREATE TABLE `{$wpdb->prefix}kanzusupport_replies` (
+				`primkey` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+				`ticket_id` INT NOT NULL ,
+				`user_id` INT NOT NULL ,
+				`timestamp` VARCHAR( 128 ) NOT NULL ,
+				`message` TEXT NOT NULL
+				);				
+			";
+
+      dbDelta( $kanzusupport_tables );
+  
+ }
+ 
+ 
+/**
+ * Add menu items in the admin panel
+ */
+function kanzu_support_menu_pages() {
+    //Add the top-level admin menu
+    $page_title = 'Kanzu Support';
+    $menu_title = 'Kanzu Support';
+    $capability = 'manage_options';
+    $menu_slug = 'kanzu-support';
+    $function = 'kanzu_support_settings';
+    add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function);
+    
+	//Add the ticket pages
+	$ticket_types = array();
+	$ticket_types['ks-my-unresolved']='My unresolved tickets';
+	$ticket_types['ks-all-tickets']='All tickets';
+	$ticket_types['ks-unassigned']='Unassigned tickets';
+	$ticket_types['ks-recently-updated']='Recently updated';
+	$ticket_types['ks-recently-resolved']='Recently resolved';
+	$ticket_types['ks-closed']='Closed';	
+    
+	foreach ( $ticket_types as $submenu_slug => $submenu_title ) {
+		$submenu_function = 'kanzu_support_tickets';
+		add_submenu_page($menu_slug, $page_title, $submenu_title, $capability, $submenu_slug, $submenu_function);
+	}
+	
+	// Add submenu page with same slug as parent to ensure no duplicates
+    $sub_menu_title = 'Settings';
+    add_submenu_page($menu_slug, $page_title, $sub_menu_title, $capability, $menu_slug, $function);
+
+    // Now add the submenu page for Help
+    $submenu_page_title = 'Kanzu Support Help';
+    $submenu_title = 'Help';
+    $submenu_slug = 'kanzu-support-help';
+    $submenu_function = 'kanzu_support_help';
+    add_submenu_page($menu_slug, $submenu_page_title, $submenu_title, $capability, $submenu_slug, $submenu_function);
+	
+}
+
+function kanzu_support_settings() {
+    if (!current_user_can('manage_options')) {
+        wp_die('You do not have sufficient permissions to access this page.');
+    }
+
+    // Render the HTML for the Settings page or include a file that does
+}
+
+function kanzu_support_help() {
+    if (!current_user_can('manage_options')) {
+        wp_die('You do not have sufficient permissions to access this page.');
+    }
+
+    // Render the HTML for the Help page or include a file that does
+}
+
+/** 
+ * Handle all the tickets requests
+ */
+ function kanzu_support_tickets($type){
+ 
+ }
+
+
+
+
+/**
+ * Add a link on the plugins page, under our plugin name, linking to our settings page. 
+ */
+function kanzu_support_plugin_action_links($links, $file) {
+    static $this_plugin;
+
+    if (!$this_plugin) {
+        $this_plugin = plugin_basename(__FILE__);
+    }
+
+    if ($file == $this_plugin) {
+        // The "page" query string value must be equal to the slug
+        // of the Settings admin page we defined earlier, which in
+        // this case equals "kanzu-support".
+        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=kanzu-support">Settings</a>';
+        array_unshift($links, $settings_link);
+    }
+
+    return $links;
+}
+
+ 
+ //Will handle updates
+ function ks_update(){
+ 
+ 
+ }
+ 
+ 
+ }
--- kanzu-support-desk.php
+++ kanzu-support-desk.php
@@ -1,32 +1,80 @@
 <?php
 /*
-  Plugin Name: Kanzu Support Desk
-  Plugin URI: http://kanzucode.com/kanzu-support-desk
-  Description: A simple support desk ticketing system for your WordPress site. 
-  Version: 1.0.0
-  Author: Kanzu Code
-  Author URI: URI: http://kanzucode.com
-  License: LGPL
-  Text Domain: kanzu-support-desk
+ * Plugin Name: Kanzu Support Desk
+ * Plugin URI: http://kanzucode.com/kanzu-support-desk
+ * Description: A simple support desk ticketing system for your WordPress site. 
+ * Version: 1.0.0
+ * Author: Kanzu Code
+ * Author URI: URI: http://kanzucode.com
+ * License: GPL2
+ * Text Domain: kanzu-support-desk
+ * 
+ *
+ * @package KanzuSupport
+ * @category Core
+ * @author KanzuCode
  */
 
- // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
-//Store the Plugin version. We'll need this for upgrades
-if (!defined('KANZU_SUPPORT_VERSION_KEY'))
-    define('KANZU_SUPPORT_VERSION_KEY', 'kanzu_support_version');
+//if ( ! class_exists( 'KanzuSupport' ) ) :
 
-if (!defined('KANZU_SUPPORT_VERSION_NUM'))
-    define('KANZU_SUPPORT_VERSION_NUM', '1.0.0');
 
-add_option(KANZU_SUPPORT_VERSION_KEY, KANZUSUPPORT_VERSION_NUM);
- 
+/**
+ * Main Kanzu Support Desk Class
+ *
+ * @class KanzuSupport
+ * @version	1.0.0
+ */
+final class KanzuSupport {
+
+	/**
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	
+	public function __construct() {
+		// Auto-load classes on demand
+		if ( function_exists( "__autoload" ) ) {
+			spl_autoload_register( "__autoload" );
+		}
+
+		spl_autoload_register( array( $this, 'autoload' ) );
+
+		// Define constants
+		$this->define_constants();
+
+		// Include required files
+		$this->includes();
+	}
+	
+	/**
+	 * Define KS Constants
+	 */
+	private function define_constants() {
+		define( 'KS_PLUGIN_FILE', __FILE__ );
+		define( 'KS_VERSION', $this->version );
+		
+		//Store the Plugin version. We'll need this for upgrades
+	if (!defined('KANZU_SUPPORT_VERSION_KEY'))
+		define('KANZU_SUPPORT_VERSION_KEY', 'kanzu_support_version');
+
+	add_option(KANZU_SUPPORT_VERSION_KEY, KS_VERSION);
+
+	}
+	
+	private function includes() {
+		include_once( 'includes/class-ks-install.php' );
+		$init = new KS_Install();
+		}
  
  /**
   * Added to write custom debug messages
   */
- function kanzu_support_log_me($message) {
+	public function kanzu_support_log_me($message) {
     if (WP_DEBUG === true) {
         if (is_array($message) || is_object($message)) {
             error_log(print_r($message, true));
@@ -37,74 +85,4 @@ add_option(KANZU_SUPPORT_VERSION_KEY, KANZUSUPPORT_VERSION_NUM);
 } 
 
 
-/**
- * Add menu items in the admin panel
- */
-
-add_action('admin_menu', 'kanzu_support_menu_pages');
-
-function kanzu_support_menu_pages() {
-    //Add the top-level admin menu
-    $page_title = 'Kanzu Support';
-    $menu_title = 'Kanzu Support';
-    $capability = 'manage_options';
-    $menu_slug = 'kanzu-support-settings';
-    $function = 'kanzu_support_settings';
-    add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function);
-
-    // Add submenu page with same slug as parent to ensure no duplicates
-    $sub_menu_title = 'Settings';
-    add_submenu_page($menu_slug, $page_title, $sub_menu_title, $capability, $menu_slug, $function);
-
-    // Now add the submenu page for Help
-    $submenu_page_title = 'Kanzu Support Help';
-    $submenu_title = 'Help';
-    $submenu_slug = 'kanzu-support-help';
-    $submenu_function = 'kanzu_support_help';
-    add_submenu_page($menu_slug, $submenu_page_title, $submenu_title, $capability, $submenu_slug, $submenu_function);
 }
-
-function kanzu_support_settings() {
-    if (!current_user_can('manage_options')) {
-        wp_die('You do not have sufficient permissions to access this page.');
-    }
-
-    // Render the HTML for the Settings page or include a file that does
-}
-
-function kanzu_support_help() {
-    if (!current_user_can('manage_options')) {
-        wp_die('You do not have sufficient permissions to access this page.');
-    }
-
-    // Render the HTML for the Help page or include a file that does
-}
-
-add_filter('plugin_action_links', 'kanzu_support_plugin_action_links', 10, 2);
-
-/**
- * Add a link on the plugins page, under our plugin name, linking to our settings page. 
- */
-function kanzu_support_plugin_action_links($links, $file) {
-    static $this_plugin;
-
-    if (!$this_plugin) {
-        $this_plugin = plugin_basename(__FILE__);
-    }
-
-    if ($file == $this_plugin) {
-        // The "page" query string value must be equal to the slug
-        // of the Settings admin page we defined earlier, which in
-        // this case equals "kanzu-support-settings".
-        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=kanzu-support-settings">Settings</a>';
-        array_unshift($links, $settings_link);
-    }
-
-    return $links;
-}
- 
- 
- 
- 
- 
- ?>
