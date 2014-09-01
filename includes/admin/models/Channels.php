1058f78f2293dacb4c242be5fb227f9a28c34499
unknown <emmanuel.ssebaggala@SAOSS00825LT.SA.AIRCOMINTERNATIONAL.com>
2014-09-01 14:29:11 +0200

    Added Channel model.

---
+++ includes/admin/models/Channels.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *
+ * @package   Kanzu_Support_Desk
+ * @author    Kanzu Code <feedback@kanzucode.com>
+ * @license   GPL-2.0+
+ * @link      http://kanzucode.com
+ * @copyright 2014 Kanzu Code
+ *
+ * Channels.php
+ */
+ 
+$plugindir = plugin_dir_path( __FILE__ );
+
+$DS=DIRECTORY_SEPARATOR;
+$plugindir = dirname(dirname(plugin_dir_path( __FILE__ )));
+include( $plugindir. $DS . "admin" . $DS."libs".$DS."Model.php");
+
+
+ class ChannelsModel extends Kanzu_Model{
+
+	
+	public function __construct(){
+		global $wpdb;
+		$this->_tablename = $wpdb->prefix . "kanzusupport_channels";	
+		$this->_id = "chan_id";
+			
+		$this->_formats = array(
+		'chan_id' 			 => '%d', 
+		'chan_chantype_id'	 => '%s',
+		'chan_handle'	 	 => '%s' , 
+		'chan_description' 	 => '%s'
+		);
+	}
+	
+	/*
+	*Get user object
+	*
+	*@param userid
+	*/
+	public function getChannel( $id ){
+		return parent::getRow($id);
+	}
+	
+	/*
+	*Get all from users (kanzu-users) from wp users table
+	*
+	*@param $filter SQL filter. Everything after the WHERE key word
+	*/
+	public  function getAll( $filter = "" ){
+		return parent::getRow($filter = "");
+	}
+ 
+	/*
+	*
+	*/
+	public function addChannel( &$channel ){
+		return parent::addRow( $channel );
+	}
+	
+	/*
+	*
+	*@param Channel object.
+	*/
+	public function deleteChannel(  &$channel ){
+		return parent::deleteRow( $channel );
+	}
+	
+
+	/*
+	* Save/update 
+	*@param Channel object
+	* *new_* for new value
+	*/
+	public function updateChannel( &$channel ){
+		return parent::updateTicket( $channel );
+	}
+ }
+ 
+ 
+ ?>
