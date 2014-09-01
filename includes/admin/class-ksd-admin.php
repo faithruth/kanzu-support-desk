d091834ee54fd94739b96cb6aa04e61fad5ea2d1
Peter Kakoma <kakomap@gmail.com>
2014-09-01 12:21:15 +0300

    Dashboard skeleton setup

--- includes/admin/class-ksd-admin.php
+++ includes/admin/class-ksd-admin.php
@@ -23,16 +23,8 @@ class Kanzu_Support_Admin {
 	 * @var      object
 	 */
 	protected static $instance = null;
+	
 
-	/**
-	 * Slug of the plugin screen.
-	 *
-	 * @since    1.0.0
-	 *
-	 * @var      string
-	 */
-	protected $plugin_screen_hook_suffix = null;	
- 
 
 	/**
 	 * Initialize the plugin by loading admin scripts & styles and adding a
@@ -89,15 +81,8 @@ class Kanzu_Support_Admin {
 	 * @return    null    Return early if no settings page is registered.
 	 */
 	public function enqueue_admin_styles() {
-
-		if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
-			return;
-		}
-
-		$screen = get_current_screen();
-		if ( $this->plugin_screen_hook_suffix == $screen->id ) {
-			wp_enqueue_style( KSD_SLUG .'-admin-styles', plugins_url( 'assets/css/admin.css', __FILE__ ), array(), KSD_VERSION );
-		}
+	
+		wp_enqueue_style( KSD_SLUG .'-admin-styles', plugins_url( '../../assets/css/admin-kanzu-support-desk.css', __FILE__ ), array(), KSD_VERSION );
 
 	}
 
@@ -109,17 +94,9 @@ class Kanzu_Support_Admin {
 	 *
 	 * @return    null    Return early if no settings page is registered.
 	 */
-	public function enqueue_admin_scripts() {
-
-		if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
-			return;
-		}
-
-		$screen = get_current_screen();
-		if ( $this->plugin_screen_hook_suffix == $screen->id ) {
-			wp_enqueue_script( KSD_SLUG . '-admin-script', plugins_url( 'assets/js/admin.js', __FILE__ ), array( 'jquery' ), KSD_VERSION );
-		}
-
+	public function enqueue_admin_scripts() { 
+		
+		wp_enqueue_script( KSD_SLUG . '-admin-script', plugins_url( '../../assets/js/admin-kanzu-support-desk.js', __FILE__ ), array( 'jquery' ), KSD_VERSION ); 
 	}
 
 	
