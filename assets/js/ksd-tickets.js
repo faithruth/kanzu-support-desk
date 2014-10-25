146c38baf4c2e57d23411bd7664b0982e0886131
Kakoma <kakomap@gmail.com>
2014-10-25 12:26:26 +0300

    Clean-up ticket grid look

--- assets/js/ksd-tickets.js
+++ assets/js/ksd-tickets.js
@@ -34,10 +34,10 @@ KSDTickets = function(){
                                                 rws = 	'<div id="ticket-list-item" class="ticket_'+value.tkt_id+'">';
                                                 rws += 	'<div class="ticket-info">';
                                                 rws += 	'<input type="checkbox" value="'+value.tkt_id+'" name="ticket_ids[]" id="ticket_checkbox_'+value.tkt_id+'">';
-
-                                                rws +=	'<span class="subject"><a href="'+ksd_admin.ksd_tickets_url+'&ticket='+value.tkt_id+'&action=edit">'+value.tkt_subject+'</a></span> - ';
-                                                rws += 	'<span class="">'+value.tkt_description+'</span>';
                                                 rws += 	'<span class="customer_name"><a href="'+ksd_admin.ksd_tickets_url+'&ticket='+value.tkt_id+'&action=edit">'+value.tkt_logged_by+'</a></span>';
+                                                rws +=	'<span class="subject-and-message-excerpt"><a href="'+ksd_admin.ksd_tickets_url+'&ticket='+value.tkt_id+'&action=edit"><strong>'+value.tkt_subject+'</strong>';
+                                                rws += 	' - '+value.tkt_description; 
+                                                rws += 	'</a></span>';
                                                 rws += 	'<span class="ticket-time">'+value.tkt_time_logged+'</span>';
                                                 rws += 	'</div>';
                                                 rws += 	'<div class="ticket-actions" id="tkt_'+value.tkt_id+'">';
