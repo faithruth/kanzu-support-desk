6bd698663c1794fdc071fd265ef71165b8ac4ec4
Peter Kakoma <kakomap@gmail.com>
2014-11-17 21:07:11 +0300

    Add license check. To add plugin deact if lic isn't active

--- addons/ksd-mail/includes/libraries/class-ksd-mail.php
+++ addons/ksd-mail/includes/libraries/class-ksd-mail.php
@@ -17,10 +17,7 @@ class Kanzu_Mail {
 
 
 	public function __construct(){
-            if( class_exists('Kanzu_Support_Desk') ){//Check that Kanzu Support Desk is active. If it is, get settings
-                $base_settings = Kanzu_Support_Desk::get_settings();
-                $this->settings = $base_settings[KSD_Mail_Install::$ksd_options_name];
-            }
+            $this->settings = KSD_Mail::get_settings();           
 	}
         
 
