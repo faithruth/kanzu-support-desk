044e504081940b7a00845a2a87b362fc259147a7
Peter Kakoma <kakomap@gmail.com>
2014-11-19 20:14:22 +0300

    Internalize settings labels

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -4,41 +4,41 @@
     <h3><?php _e("General","kanzu-support-desk"); ?> </h3>
         <div>
              <div class="setting">
-                <label for="enable_new_tkt_notifxns">Enable new ticket notifications</label>
+                <label for="enable_new_tkt_notifxns"><?php _e( "Enable new ticket notifications","kanzu-support-desk" ); ?></label>
                 <img width="16" height="16" src="<?php echo KSD_PLUGIN_URL."/assets/images/help.png";?>" class="help_tip" title="<?php _e("If this is enabled, an email is sent to the customer's email address for all new tickets logged from the front-end",'kanzu-support-desk')  ;?>"/>
                 <input name="enable_new_tkt_notifxns"  type="checkbox" <?php checked( $settings['enable_new_tkt_notifxns'], "yes" ) ?> value="yes"  />
              </div>
              <div class="enable_new_tkt_notifxns">
                 <div class="setting">
-                   <label for="ticket_mail_from_name">From (Name)</label>
+                   <label for="ticket_mail_from_name"><?php _e( "From (Name)","kanzu-support-desk" ); ?></label>
                    <img width="16" height="16" src="<?php echo KSD_PLUGIN_URL."/assets/images/help.png";?>" class="help_tip" title="<?php _e("Defaults to the primary administrator's display name",'kanzu-support-desk')  ;?>"/>
                    <input type="text" value="<?php echo $settings['ticket_mail_from_name']; ?>" size="30" name="ticket_mail_from_name" />
                </div>
                <div class="setting">
-                   <label for="ticket_mail_from_email">From (Email Address)</label>
+                   <label for="ticket_mail_from_email"><?php _e( "From (Email Address)","kanzu-support-desk" ); ?></label>
                    <img width="16" height="16" src="<?php echo KSD_PLUGIN_URL."/assets/images/help.png";?>" class="help_tip" title="<?php _e("Defaults to the primary administrator's email address",'kanzu-support-desk')  ;?>"/>
                    <input type="text" value="<?php echo $settings['ticket_mail_from_email']; ?>" size="30" name="ticket_mail_from_email" />
                </div>
                <div class="setting">
-                   <label for="ticket_mail_subject">Subject</label>
+                   <label for="ticket_mail_subject"><?php _e( "Subject","kanzu-support-desk" ); ?></label>
                    <input type="text" value="<?php echo $settings['ticket_mail_subject']; ?>" size="60" name="ticket_mail_subject" />
                </div>
                <div class="setting">
-                   <label for="ticket_mail_message">Message</label>
+                   <label for="ticket_mail_message"><?php _e( "Message","kanzu-support-desk" ); ?></label>
                    <textarea cols="60" rows="4" name="ticket_mail_message"><?php echo $settings['ticket_mail_message']; ?></textarea>
                </div>
              </div><!--.enable_new_tkt_notifxns-->
             <div class="setting">
-                <label for="recency_definition">Recency Definition ( In Hours ) </label>
+                <label for="recency_definition"><?php _e( "Recency Definition ( In Hours )","kanzu-support-desk" ); ?></label>
                 <img width="16" height="16" src="<?php echo KSD_PLUGIN_URL."/assets/images/help.png";?>" class="help_tip" title="<?php _e("In the ticket view, the 'Recently Updated' & 'Recently resolved' tabs, show tickets updated in last X hours",'kanzu-support-desk')  ;?>"/>
                 <input type="text" value="<?php echo $settings['recency_definition']; ?>" size="15" name="recency_definition" />
             </div>
             <div class="setting">
-                <label for="show_support_tab">Show front-end support tab</label>
+                <label for="show_support_tab"><?php _e( "Show front-end support tab","kanzu-support-desk" ); ?></label>
                 <input name="show_support_tab"  type="checkbox" <?php checked( $settings['show_support_tab'], "yes" ) ?> value="yes"  />
              </div>
              <div class="setting show_support_tab">
-                   <label for="tab_message_on_submit">Tab message on ticket submission</label>
+                   <label for="tab_message_on_submit"><?php _e( "Tab message on ticket submission","kanzu-support-desk" ); ?></label>
                    <textarea cols="60" rows="4" name="tab_message_on_submit"><?php echo $settings['tab_message_on_submit']; ?></textarea>
              </div>   
         </div>   
