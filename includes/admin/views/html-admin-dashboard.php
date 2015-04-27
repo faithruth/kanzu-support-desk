fcce30eab438b5455d52ab92bbad0d149c2f8778
Peter Kakoma <kakomap@gmail.com>
2015-04-27 21:01:27 +0300

    KSD version 1.5.4. Github initial commit

---
+++ includes/admin/views/html-admin-dashboard.php
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+<ul class="dashboard-statistics-summary pending">
+        <li> <?php _e( 'Loading...', 'kanzu-support-desk'); ?></li>
+</ul>
+
+<div id="ksd_dashboard_chart"> <!--Ticket Inflow-->   
+</div>
+
+<!--Tickets by status-->     <!--Tickets by channel-->
+	
+
+=======
+<ul class="dashboard-statistics-summary pending">
+        <li> <?php _e( 'Loading...', 'kanzu-support-desk'); ?></li>
+</ul>
+
+<div id="ksd_dashboard_chart"> <!--Ticket Inflow-->   
+</div>
+
+<!--Tickets by status-->     <!--Tickets by channel-->
+	
+
+>>>>>>> 165e6af137319d117111e009717725b2c4b68d84
+<!--Top Agents-->
