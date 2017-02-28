37d376bb3ff2f1fc7fb1e3009a9193b057035797
Peter Kakoma <kakoma@kanzucode.com>
2017-02-28 08:00:38 +0300

    Add initial getting-started page

---
+++ assets/js/ksd-gs.js
@@ -0,0 +1,8 @@
+ (function($) {
+    $(document).ready(function () {
+     	$( function() {
+    	$( "#ksd-gs-tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    	$( "#ksd-gs-tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+  	});
+});
+ })(jQuery);
--- includes/class-ksd-onboarding.php
+++ includes/class-ksd-onboarding.php
@@ -67,11 +67,53 @@ class KSD_Onboarding {
             add_action( 'ksd_show_onboarding_progress', array( $this, 'show_onboarding_progress' ) );   
             
             add_action( 'admin_notices', array( $this, 'show_onboarding_progress' ) );
+
+            if( isset( $_GET['ksd_getting_started'] ) ):
+                add_action( 'admin_notices', array( $this, 'render_getting_started_banner' ) );
+                add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_getting_started_scripts' ) );
+                add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_getting_started_styles' ) );
+            endif;    
             
             //Jubilate when we are done
             add_action( 'ksd_onboarding_complete', array( $this, 'onboarding_complete' ) );
         }
-        
+
+        /**
+         * Render a getting started message
+         * @return string
+         */
+        public function render_getting_started_banner(){?>
+            <div id="ksd-gs-tabs">
+              <ul>
+                <li><a href="#ksd-gs-tabs-1">Nunc tincidunt</a></li>
+                <li><a href="#ksd-gs-tabs-2">Proin dolor</a></li>
+                <li><a href="#ksd-gs-tabs-3">Aenean lacinia</a></li>
+              </ul>
+              <div id="ksd-gs-tabs-1">
+                <h2>Content heading 1</h2>
+                <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+              </div>
+              <div id="ksd-gs-tabs-2">
+                <h2>Content heading 2</h2>
+                <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+              </div>
+              <div id="ksd-gs-tabs-3">
+                <h2>Content heading 3</h2>
+                <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+                <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+              </div>
+            </div>
+            <?php
+        }        
+
+        public function enqueue_getting_started_scripts(){
+            wp_enqueue_script( KSD_SLUG . '-gs-js', KSD_PLUGIN_URL.'/assets/js/ksd-gs.js', array( 'jquery', 'jquery-ui-core', 'jquery-ui-tabs' ), KSD_VERSION );
+
+        }
+
+        public function enqueue_getting_started_styles(){
+            wp_enqueue_style( KSD_SLUG .'-gs-css', KSD_PLUGIN_URL.'/assets/css/ksd-gs.css' );            
+        }        
         
         /**
          * Show the onboarding progress
