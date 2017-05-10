9206cf7d7577eb641c3fdf07f08cc9e126931aec
Peter Kakoma <kakoma@kanzucode.com>
2017-05-10 04:58:26 +0300

    Setup help for CF7

--- templates/admin/contact-form7-panel.php
+++ templates/admin/contact-form7-panel.php
@@ -7,26 +7,30 @@
 </p>
 <div class="ksd-wpcf7-config-wrapper">
 	<p>
-		<?php _e( 'Use these tags to specify which fields to use for the support ticket name, email, subject and message.','kanzu-support-desk' );
+		<?php _e( "Use these tags to specify which fields to use for the support ticket name, email, subject and message. We auto-populate these fields for you based on Contact form 7's default tags.",'kanzu-support-desk' );
 	?>
 	</p>
 	<p><?php echo $cf7_suggested_mail_tags; ?></p>
-	<form class="ksd-contact-form-7"> 
-	        <div class="ksd-cf7-field ksd-cust-fullname-cf7">       
+	<form class="ksd-contact-form-7">
+	        <div class="ksd-cf7-field ksd-cust-fullname-cf7">
 	          <label for="_ksd_wpcf7_name"><?php _e( 'Name', 'kanzu-support-desk' ); ?></label>
 	          <input type="text" name="_ksd_wpcf7_name" class="ksd-cust-fullname large-text code" value="<?php echo get_post_meta( $cf7_form->id, '_ksd_wpcf7_name', true );?>"/>
 	        </div>
-	        <div class="ksd-cf7-field ksd-customer-email-cf7">       
+	        <div class="ksd-cf7-field ksd-customer-email-cf7">
 	        	<label for="_ksd_wpcf7_email"><?php _e( 'Email', 'kanzu-support-desk' ); ?></label>
 	          	<input type="text" name="_ksd_wpcf7_email" class="ksd-customer-email large-text code" value="<?php echo get_post_meta( $cf7_form->id, '_ksd_wpcf7_email', true );?>"/>
-	        </div>              
-	    	<div class="ksd-cf7-field ksd-subject-cf7"> 
-	    		<label for="_ksd_wpcf7_subject"><?php _e( 'Subject', 'kanzu-support-desk' ); ?></label>      
+	        </div>
+	    	<div class="ksd-cf7-field ksd-subject-cf7">
+	    		<label for="_ksd_wpcf7_subject"><?php _e( 'Subject', 'kanzu-support-desk' ); ?></label>
 	      		<input type="text" name="_ksd_wpcf7_subject" class="ksd-subject large-text code" value="<?php echo get_post_meta( $cf7_form->id, '_ksd_wpcf7_subject', true );?>" />
 	    	</div>
-	    	<div class="ksd-cf7-field ksd-message-cf7"> 
-	    		<label for="_ksd_wpcf7_message"><?php _e( 'Message', 'kanzu-support-desk' ); ?></label>      
+	    	<div class="ksd-cf7-field ksd-message-cf7">
+	    		<label for="_ksd_wpcf7_message"><?php _e( 'Message', 'kanzu-support-desk' ); ?></label>
 	      		<input type="text" name="_ksd_wpcf7_message" class="ksd-message large-text code" value="<?php echo get_post_meta( $cf7_form->id, '_ksd_wpcf7_message', true );?>"/>
-	    	</div>    	
+	    	</div>
 	</form>
+	<p>
+		<?php _e( 'To test, send a message using the form and check that a ticket is created', 'kanzu-support-desk' ); ?>
+	</p>
+	<a href="https://kanzucode.com/knowledge_base/ksd-contact-form-7-integration-quickstart/" target="_blank"><?php _e( 'Configuration Help', 'kanzu-support-desk'); ?></a>
 </div>
