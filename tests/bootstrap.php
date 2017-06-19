5c9786f348cddab793913ddb92580016332d6ea3
Peter Kakoma <kakoma@kanzucode.com>
2017-06-19 17:13:49 +0300

    Add WPMock to tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,12 +5,12 @@
  * @package Kanzu_Support_Desk
  */
 
-		/** 
- 	 	 * Migration fixer for PHPUnit 6 
- 	 	 */ 
- 	 	if ( class_exists( 'PHPUnit\Runner\Version' ) ) { 
- 	 	        require_once dirname( __FILE__ ) . '/phpunit6-compat.php'; 
- 	 	} 
+		/**
+ 	 	 * Migration fixer for PHPUnit 6
+ 	 	 */
+ 	 	if ( class_exists( 'PHPUnit\Runner\Version' ) ) {
+ 	 	        require_once dirname( __FILE__ ) . '/phpunit6-compat.php';
+ 	 	}
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
@@ -19,6 +19,7 @@ if ( ! $_tests_dir ) {
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
+
 
 /**
  * Manually load the plugin being tested.
@@ -34,8 +35,12 @@ require $_tests_dir . '/includes/bootstrap.php';
 echo PHP_EOL;
 echo "Activating Kanzu Support Desk...\n";
 activate_plugin( 'kanzu-support-desk/kanzu-support-desk.php' );
+require_once( KSD_PLUGIN_DIR. 'includes/vendor/autoload.php' );
+
+WP_Mock::bootstrap();//Warm up
 
 require_once( KSD_PLUGIN_DIR . 'includes/admin/class-ksd-admin.php' );
+
 
 echo PHP_EOL;
 echo 'KSD_VERSION: ' . KSD_VERSION;
